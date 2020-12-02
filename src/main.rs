fff6ccb1009af0f9442d618fb80bac04dfcb1da3
Jérémy DEMANGE <j@lachainemedias.com>
2020-12-02 01:37:03 +0100

    Add folder

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate whatlang;
+
+use whatlang::{detect, Lang, Script};
+
 fn main() {
-    println!("Hello, world!");
+    let text = "Bonjour à tous, j'espère que vous allez bien !";
+    let info = detect(text).unwrap();
+    println!("{}", info.lang());
+    println!("{}", info.confidence());
 }
